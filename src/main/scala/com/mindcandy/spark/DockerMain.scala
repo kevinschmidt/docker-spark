cfaca7270520a95bf5e1975e76093c5c3988f788
Kevin Schmidt <kevin@kevinschmidt.biz>
2014-11-21 10:43:47 +0100

    basic test app

---
+++ src/main/scala/com/mindcandy/spark/DockerMain.scala
@@ -0,0 +1,17 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object DockerMain {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("Docker Spark").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val NUM_SAMPLES = 10
+    val count = sc.parallelize(1 to NUM_SAMPLES).map { i =>
+      val x = Math.random()
+      val y = Math.random()
+      if (x * x + y * y < 1) 1 else 0
+    }.reduce(_ + _)
+    println("Pi is roughly " + 4.0 * count / NUM_SAMPLES)
+  }
+}
